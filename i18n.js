f17aff9b08c1170c17994d204ed609b23c73ede6
fabien-heyte <fabien.heyte@gmail.com>
2014-04-22 10:56:53 +0200

    traduction
    traduction des différentes spécificités de l'étage (escalier, ascenseur
    ...) en plusieurs langage

--- i18n.js
+++ i18n.js
@@ -3,231 +3,203 @@
  *  KEEP alphabetic order 
  */
 
-var translation = new array(
+	var translation = {
     /* 
      * Website 
      * -------------------------------------------------------------------------
      */
-    Info = new array(
-        'fr' : 'Informations',
-        'it' : 'Informazioni',
-        'pl' : 'Informacje'
-    ),
-    Description = new array(
+    'Info' :{
+        'fr': 'Informations',
+        'it': 'Informazioni',
+        'pl': 'Informacje'
+    },
+    'Description' :{
+		'fr' : 'Description',
         'it' : 'Descrizione',
         'pl' : 'Opis'
-    ),
-    Model implementation = new array(
+    },
+    'Model implementation' :{
+		'fr' : 'Implementation Modele',
         'it' : 'Implementazione del modello',
         'pl' : 'Implementacja modelu'
-    ),
-    Contact = new array(
+    },
+    'Contact' :{
         'de' : 'Kontakt',
         'fr' : 'Contact',
         'it' : 'Contatti',
         'pl' : 'Kontakt'
-    ),
-    More = new array(
+    },
+    'More' :{
         'de' : 'Mehr',
         'fr' : 'Plus',
         'it' : 'Altro',
         'pl' : 'Więcej'
-    )
+    },
     
-  /*  'Map' => array(
-        'de' => 'Karte',
-        'fr' => 'Carte',
-        'it' => 'Mappa',
-        'pl' => 'Mapa'
-    ),
-    'Click to load buildings' => array(
-        'pl' => 'Kliknij aby załadować budynki'
-    ),
-    '<strong>Zoom in</strong> to load buildings' => array(
-        'fr' => '<strong>Zoomer</strong> pour charger les bâtiments',
-        'it' => '<strong>Ingrandisci lo zoom</strong> per caricare gli edifici',
-        'pl' => '<strong>Przybliż mapę</strong> aby pobrać elementy'
-    ),
+    'Map' :{
+        'de' : 'Karte',
+        'fr' : 'Carte',
+        'it' : 'Mappa',
+        'pl' : 'Mapa'
+    },
+    'Click to load buildings' :{
+		'fr' : 'Cliquer pour charger le batiment',
+        'pl' : 'Kliknij aby załadować budynki'
+    },
+    '<strong>Zoom in</strong> to load buildings' :{
+        'fr' : '<strong>Zoomer</strong> pour charger les bâtiments',
+        'it' : '<strong>Ingrandisci lo zoom</strong> per caricare gli edifici',
+        'pl' : '<strong>Przybliż mapę</strong> aby pobrać elementy'
+    },
     
     /* 
      * Navigation 
      * -------------------------------------------------------------------------
-     
-    'Navigation' => array(
-        'fr' => 'Navigation',
-        'it' => 'Navigazione',
-        'pl' => 'Nawigacja'
-    ),
+     */
+	
+    'Navigation' :{
+        'fr' : 'Navigation',
+        'it' : 'Navigazione',
+        'pl' : 'Nawigacja'
+    },
     
     // Shops
-    'All' => array(
-        'de' => 'Tout',
-        'fr' => 'Tous',
-        'it' => 'Tutti',
-        'pl' => 'Wszystko'
-    ),
-    'Fashion' => array(
-        'fr' => 'Mode',
-        'it' => 'Moda',
-        'pl' => 'Moda'
-    ),
-    'Home' => array(
-        'fr' => 'Maison',
-        'it' => 'Casa',
-        'pl' => 'Wyposażenie domu',
-    ),
-    'Electronics' => array(
-        'fr' => 'Electronique',
-        'it' => 'Elettronica',
-        'pl' => 'Elektronika',
-    ),
-    'Health' => array(
-        'fr' => 'Santé',
-        'it' => 'Salute',
-        'pl' => 'Zdrowie'
-    ),
-    'Food' => array(
-        'fr' => 'Alimentation',
-        'it' => 'Alimentari',
-        'pl' => 'Spożywcze'
-    ),
-    'Service' => array(
-        'fr' => 'Service',
-        'it' => 'Servizi',
-        'pl' => 'Usługi'
-    ),
-    'Gastronomy' => array(
-        'fr' => 'Gastronomie',
-        'it' => 'Gastronomia',
-        'pl' => 'Gastronomia'
-    ),
-    'Sport' => array(
-        'fr' => 'Sport',
-        'it' => 'Sport',
-        'pl' => 'Sport'
-    ),
-    'Entertainment' => array(
-        'fr' => 'Divertissement',
-        'it' => 'Intrattenimento',
-        'pl' => 'Rozrywka'
-    ),
-    'Other' => array(
-        'fr' => 'Autre',
-        'it' => 'Altro',
-        'pl' => 'Inne'
-    ), 
+    'WC' :{
+        'de': 'WC',
+        'fr': 'WC',
+        'it': 'WC',
+        'pl': 'Ubikacja'
+    },
+    'Elevator' : {
+        'fr': 'Ascenseur',
+        'it': 'Sollevamento',
+        'pl': 'wyciąg'
+    },
+    'Stairs' : {
+        'fr': 'Escaliers',
+        'it': "Scala",
+        'pl': 'Schody',
+    },
     
     // empty
-    'Empty floor' => array(
-        'it' => 'Piano vuoto',
-        'pl' => 'Puste piętro'
-    ),
-    'None on this floor' => array(
-        'it' => 'Niente su questo piano',
-        'pl' => 'Brak na piętrze'
-    ), 
+    'Empty floor' : {
+		'fr' : 'Etage vide',
+        'it' : 'Piano vuoto',
+        'pl' : 'Puste piętro'
+    },
+    'None on this floor' : {
+		'fr' : 'Rien sur cette etage' ,
+        'it' : 'Niente su questo piano',
+        'pl' : 'Brak na piętrze'
+    }, 
     
     /* 
      * Modal 
      * -------------------------------------------------------------------------
-     
-    'Type' => array(
-        'fr' => 'Type',
-        'it' => 'Tipo',
-        'pl' => 'Typ'
-    ),
+     */
+	 
+    'Type' : {
+        'fr' : 'Type',
+        'it' : 'Tipo',
+        'pl' : 'Typ'
+    },
     
-    'Edit in JOSM' => array(
-        'fr' => 'Editer avec JOSM',
-        'it' => 'Modifica in JOSM',
-        'pl' => 'Edytuj w JOSM'
-    ),
+    'Edit in JOSM' : {
+        'fr' : 'Editer avec JOSM',
+        'it' : 'Modifica in JOSM',
+        'pl' : 'Edytuj w JOSM'
+    },
   
-    'Close' => array(
-        'fr' => 'Fermer',
-        'it' => 'Chiudi',
-        'pl' => 'Zamknij',
-    ),
+    'Close': {
+        'fr' : 'Fermer',
+        'it' : 'Chiudi',
+        'pl' : 'Zamknij',
+    },
     
     /*
      * Contacts section
-     
-
-    'email' => array(
-      'fr' => 'mél',
-      'it' => 'email',
-      'pl' => 'email',
-    ),
+     */
+	
+    'email' : {
+      'fr' : 'mél',
+      'it' : 'email',
+      'pl' : 'email',
+    },
   
-    'fax' => array(
-      'fr' => 'fax',
-      'it' => 'fax',
-      'pl' => 'fax',
-    ),
+    'fax' : {
+      'fr' : 'fax',
+      'it' : 'fax',
+      'pl' : 'fax',
+    },
   
-    'phone' => array(
-      'fr' => 'tél',
-      'it' => 'tel',
-      'pl' => 'tel',
-    ),
+    'phone' : {
+      'fr' : 'tél',
+      'it' : 'tel',
+      'pl' : 'tel',
+    },
   
-    'website' => array(
-      'fr' => 'site',
-      'it' => 'sito',
-      'pl' => 'www',
-    ),
+    'website' : {
+      'fr' : 'site',
+      'it' : 'sito',
+      'pl' : 'www',
+    },
     
     /*
      * Opening hours section
      *
+	 */
     
-    'Opening hours' => array(
-      'it' => 'Orari di apertura',
-      'pl' => 'Godziny otwarcia',
-    ),
+    'Opening hours' : {
+	  'fr' : 'Horaires Ouvertures',
+      'it' : 'Orari di apertura',
+      'pl' : 'Godziny otwarcia',
+    },
     
-    'closed' => array(
-      'fr' => 'fermé',
-      'it' => 'chiuso',
-      'pl' => 'zamknięte',
-    ),
+    'closed' : {
+      'fr' : 'fermé',
+      'it' : 'chiuso',
+      'pl' : 'zamknięte',
+    },
     
     // date format as used by jquery-dateFormat plugin: http://archive.plugins.jquery.com/project/jquery-dateFormat
-    'dd/MM/yyyy' => array(
-      'fr' => 'dd-MM-yyyy',
-      'it' => 'dd/MM/yyyy',
-      'pl' => 'dd.MM.yyyy',
-    ),
+    'dd/MM/yyyy' : {
+      'fr' : 'dd-MM-yyyy',
+      'it' : 'dd/MM/yyyy',
+      'pl' : 'dd.MM.yyyy',
+    },
     
     // time format as used by jquery-dateFormat plugin: http://archive.plugins.jquery.com/project/jquery-dateFormat
-    'hh:mm a' => array(
-      'fr' => 'HH:mm',
-      'it' => 'HH:mm',
-      'pl' => 'HH:mm',
-    ),
-
+    'hh:mm a' : {
+      'fr' : 'HH:mm',
+      'it' : 'HH:mm',
+      'pl' : 'HH:mm',
+    },
+	
     /* 
      * Popup 
      * -------------------------------------------------------------------------
-     *
-    'Open in OSM' => array(
-        'fr' => 'Ouvrir dans OSM',
-        'it' => 'Apri in OSM',
-        'pl' => 'Zobacz w OSM'
-    ),
-    'Enter' => array(
-        'de' => 'Betreten',
-        'fr' => 'Entrer',
-        'it' => 'Entra',
-        'pl' => 'Wejdź'
-    ),
+     */
+	
+    'Open in OSM' : {
+        'fr' : 'Ouvrir dans OSM',
+        'it' : 'Apri in OSM',
+        'pl' : 'Zobacz w OSM'
+    },
+    'Enter' : {
+        'de' : 'Betreten',
+        'fr' : 'Entrer',
+        'it' : 'Entra',
+        'pl' : 'Wejdź'
+    },
     
     /* 
      * Misc
      * -------------------------------------------------------------------------
-     *
-    'no name' => array(
-        'it' => 'senza nome',
-        'pl' => 'brak nazwy'
-    )*/
-);
+     */
+	 
+    'no name': {
+		'fr' : 'pas de nom',
+        'it' : 'senza nome',
+        'pl' : 'brak nazwy'
+    }
+};
